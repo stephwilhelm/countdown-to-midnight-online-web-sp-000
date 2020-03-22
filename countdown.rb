ece1bcccc1bfdb735f1eadadcd8e84cd71fa6dd3
Steph W <wilhelm.steph@gmail.com>
2020-03-22 00:34:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,14 +2,14 @@
 def countdown(number)
   number = 10
   while number > 0
-  	puts "#{number] SECOND(S)!"
+  	puts "#{number} SECOND(S)!"
   	number -= 1
   end
   "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
-  number > 0
+  while number > 0
     puts "#{number} SECOND(S)!"
     sleep 1.seconds
     number -= 1
